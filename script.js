626fe297283809e421ba299986878bf7a6adf107
Jamsilla <jmansilla2025@gmail.com>
2024-06-04 03:21:14 -0400

    Changes:
    - Update Layout
    - Added Difficulty Menu
    
    Layout is complete!
    
    Next Update:
    
    - Add functionality to the difficulty menu
    - Possible animations (possibly later update)

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 var buttons = document.getElementsByClassName("gameButton");
 var resetWinsButton = document.getElementById("resetWinsButton");
 var resetBoardButton = document.getElementById("resetBoardButton");
+var pvpButton = document.getElementById("pvpButton");
+var pveButton = document.getElementById("pveButton");
+var easyButton = document.getElementById("easyButton");
+var mediumButton = document.getElementById("mediumButton");
+var impossibleButton = document.getElementById("impossibleButton");
 
 var xWinsText = document.getElementById("xScore");
 var oWinsText = document.getElementById("oScore");
+
+var difficultyDiv = document.getElementById("difficultyDiv");
 
 var currentChar = "X";
 var xWins = 0;
 var oWins = 0;
 var finished = false;
+
+//setting pvp as initial gamemode
+var gamemode = "pvp";
+difficultyDiv.style.display = "none";
+pvpButton.style.borderWidth = "4px";
+//setting easy as initial difficulty
+var difficuly = "easy";
+easyButton.style.borderWidth = "4px";
 
 Array.from(buttons).forEach(button => {
     button.addEventListener("click", function() {
@@ -46,7 +61,40 @@ resetBoardButton.addEventListener("click",function(){
     })
 })
 
+pvpButton.addEventListener("click",function(){
+    pvpButton.style.borderWidth = "4px";
+    difficultyDiv.style.display = "none";
+    pveButton.style.borderWidth = "1px";
+    gamemode = "pvp";
+})
 
+pveButton.addEventListener("click",function(){
+    pveButton.style.borderWidth = "4px";
+    difficultyDiv.style.display = "block";
+    pvpButton.style.borderWidth = "1px";
+    gamemode = "pve";
+})
+
+easyButton.addEventListener("click",function(){
+    easyButton.style.borderWidth = "4px";
+    mediumButton.style.borderWidth = "1px";
+    impossibleButton.style.borderWidth = "1px";
+    difficuly = "easy";
+})
+
+mediumButton.addEventListener("click",function(){
+    easyButton.style.borderWidth = "1px";
+    mediumButton.style.borderWidth = "4px";
+    impossibleButton.style.borderWidth = "1px";
+    difficuly = "medium";
+})
+
+impossibleButton.addEventListener("click",function(){
+    easyButton.style.borderWidth = "1px";
+    mediumButton.style.borderWidth = "1px";
+    impossibleButton.style.borderWidth = "4px";
+    difficuly = "impossible";
+})
 
 
 function checkWin(){
